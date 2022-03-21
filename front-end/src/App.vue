0ea24b89ce648347db35a8f3881dd37e25bff912
legavin1023 <legavin1023@gmail.com>
2022-03-21 10:59:35 +0900

    scss

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,15 +1,26 @@
 <template>
   <div id="app">
+    <div id="nav">
+      <router-link to="/main"><h1 id="logo">logo</h1></router-link>
+      <div v-if="!isLoggedin">
+        <router-link to="/auth/sign">회원가입</router-link>
+        <router-link to="/auth/login">로그인</router-link>
+      </div>
+      <div v-if="isLoggedin">
+        <router-link to="/my-page" @click="onClick">마이 페이지</router-link>
+        <router-link to="/auth/logout">로그아웃</router-link>
+      </div>
+      <div class="data">
+        <router-link to="/dashboard">대쉬보드</router-link>
+        <router-link to="/edukit">Edukit 제어</router-link>
+      </div>
+    </div>
+    <router-view />
   </div>
 </template>
 
 <script>
-
-export default {
- 
-}
+export default {}
 </script>
 
-<style>
-
-</style>
+<style lang="scss"></style>

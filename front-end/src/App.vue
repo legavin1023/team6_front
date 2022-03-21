0f36e4093ad6f76f403596f0c79f967804d1ee26
legavin1023 <legavin1023@gmail.com>
2022-03-21 15:27:36 +0900

    대쉬보드

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -2,16 +2,16 @@
   <div id="app">
     <div id="nav">
       <!-- <router-link to="/main"><h1 id="logo">logo</h1></router-link> -->
-      <div v-if="!isLoggedin">
-        <router-link to="/auth/sign">회원가입</router-link>
+      <div v-if="!isLoggedin" class="nav_box">
+        <router-link to="/auth/sign">회원가입</router-link><br /><br />
         <router-link to="/auth/login">로그인</router-link>
       </div>
-      <div v-if="isLoggedin">
+      <div v-if="isLoggedin" class="nav_box">
         <router-link to="/my-page" @click="onClick">마이 페이지</router-link>
         <router-link to="/auth/logout">로그아웃</router-link>
       </div>
-      <div class="data">
-        <router-link to="/dashboard">대쉬보드</router-link>
+      <div id="data">
+        <router-link to="/dashboard">대쉬보드</router-link><br /><br />
         <router-link to="/edukit">Edukit 제어</router-link>
       </div>
     </div>
@@ -24,6 +24,29 @@ export default {}
 </script>
 <style lang="scss">
 #nav {
+  z-index: 99;
   position: fixed;
+  padding-left: 40px;
+  a {
+    color: $light;
+    text-decoration: none;
+    line-height: 40px;
+  }
+  a:hover {
+    font-weight: bold;
+    color: $sub1;
+  }
+  .nav_box {
+    position: fixed;
+    top: 5%;
+  }
+  #data {
+    position: fixed;
+    bottom: 5%;
+  }
+}
+body {
+  font-family: 'NEXON Lv2 Gothic';
+  background: #000;
 }
 </style>

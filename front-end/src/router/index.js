199bd0832cde52d1ae24fa1b0544247ce011db85
kce <lock6241@naver.com>
2022-03-21 10:34:16 +0900

    Vue 초기환경 재설정

---
+++ front-end/src/router/index.js
@@ -0,0 +1,57 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    redirect: '/home',
+    component: () => import('../views/edukit'),
+    children: [
+      {
+        path: '/home',
+        component: () => import('../views/edukit')
+      }
+    ]
+  },
+  {
+    path: '/dashboard',
+    component: () => import('../views/dashboard')
+  },
+  {
+    path: '/auth',
+    component: () => import('../views/auth'),
+    children: [
+      {
+        path: '/auth/login',
+        component: () => import('../views/auth/login'),
+        meta: { header: false }
+      },
+      {
+        path: '/auth/sign',
+        component: () => import('../views/auth/signup'),
+        meta: { header: false }
+      },
+      {
+        path: '/auth/logout',
+        component: () => import('../views/auth/logout'),
+        meta: { header: false }
+      }
+    ]
+  },
+  {
+    // path: '*'는 not found route 처리를 위한 것으로 항상 routes 배열 최하단에 위치해 있어야 합니다.
+    // path: '*',
+    // component: () => import('../components/NotFound.vue'),
+    // meta: { header: false }
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router

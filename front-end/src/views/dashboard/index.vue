d0631bcd616f549b59fa27b6d568b2dbfb261534
legavin1023 <legavin1023@gmail.com>
2022-03-25 16:41:55 +0900

    대쉬보드

--- front-end/src/views/dashboard/index.vue
+++ front-end/src/views/dashboard/index.vue
@@ -1,30 +1,51 @@
 <template>
-  <div class="main box">
-    <div class="text_box">
+  <div class="main">
+    <div class="text_box wrap">
       <div>
-        <p>3호기 x 좌표값 : {{}}</p>
-        <p>3호기 y 좌표값 : {{}}</p>
+        <p>1호기 x 좌표값 : {{}}</p>
+        <p>2호기 x 좌표값 : {{}}</p>
       </div>
       <div>
-        <p>하루 총 생산량</p>
+        <p>내보낸 상자 수</p>
         <p>{{}}</p>
       </div>
       <div>
-        <p>3호기 가동 시작 : {{}}</p>
-        <p>3호기 가동 종료 : {{}}</p>
-      </div>
-      <div class="clack">
-        <span>시계아이콘</span>
-        <p>작동 시간</p>
+        <p>인식한 상자 수</p>
         <p>{{}}</p>
       </div>
+      <div>
+        <p>내보낸 주사위 수</p>
+        <p>{{}}</p>
+      </div>
+      <div class="donut_chart"><PieChart_3 style="height: 160px; width: 245px" /></div>
     </div>
-    <div class="line_chart"><AreaChart style="height: 280px; width: 980px" /></div>
-    <div class="chart_box">
-      <div class="donut_chart">
-        <PieChart_1 style="height: 160px; width: 245px" /><PieChart_2 style="height: 160px; width: 245px" />
+    <div class="box">
+      <div class="text_box">
+        <div>
+          <p>3호기 x 좌표값 : {{}}</p>
+          <p>3호기 y 좌표값 : {{}}</p>
+        </div>
+        <div>
+          <p>하루 총 생산량</p>
+          <p>{{}}</p>
+        </div>
+        <div>
+          <p>3호기 가동 시작 : {{}}</p>
+          <p>3호기 가동 종료 : {{}}</p>
+        </div>
+        <div class="clack">
+          <span>시계아이콘</span>
+          <p>작동 시간</p>
+          <p>{{}}</p>
+        </div>
       </div>
-      <div class="bar_chart"><BarChart style="height: 190px; width: 470px" /></div>
+      <div class="line_chart"><AreaChart style="height: 260px; width: 980px" /></div>
+      <div class="chart_box">
+        <div class="donut_chart">
+          <PieChart_1 style="height: 160px; width: 245px" /><PieChart_2 style="height: 160px; width: 245px" />
+        </div>
+        <div class="bar_chart"><BarChart style="height: 190px; width: 470px" /></div>
+      </div>
     </div>
   </div>
 </template>
@@ -33,6 +54,8 @@
 import AreaChart from './AreaChart.vue'
 import PieChart_1 from './PieChart_1.vue'
 import PieChart_2 from './PieChart_2.vue'
+import PieChart_3 from './PieChart_3.vue'
+
 import BarChart from './BarChart.vue'
 
 export default {
@@ -41,6 +64,7 @@ export default {
     AreaChart,
     PieChart_1,
     PieChart_2,
+    PieChart_3,
     BarChart
   },
   data() {
@@ -55,19 +79,57 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.box {
+.main {
   display: flex;
-  flex-direction: column;
-  align-items: center;
   justify-content: center;
+
+  .box {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
+  .line_chart {
+    border: 3px solid $sub3;
+    width: 1000px;
+    height: 280px;
+    margin-left: -20px;
+    margin-top: 20px;
+    border-radius: 20px;
+    display: flex;
+    justify-content: center;
+  }
+  .chart_box {
+    width: 1000px;
+    margin-left: -20px;
+    margin-top: 20px;
+    display: flex;
+    justify-content: center;
+  }
+  .donut_chart,
+  .bar_chart {
+    display: flex;
+    justify-content: center;
+    width: 490px;
+    border: 3px solid $sub3;
+    border-radius: 20px;
+    height: 200px;
+  }
+  .donut_chart {
+    display: flex;
+    justify-content: center;
+    margin-right: 20px;
+    display: flex;
+    padding-top: 10px;
+  }
   .text_box {
     display: flex;
-    margin-top: 2.5%;
+    margin-top: 40px;
     > div {
       display: inline-block;
       border: 3px solid $sub3;
       color: $light;
-      width: 230px;
+      width: 235px;
       background: linear-gradient(45deg, rgba(136, 139, 191, 0.3) 0%, #ffffff00 100%);
       height: 80px;
       transition-duration: 1s;
@@ -99,37 +161,18 @@ export default {
       background: url('../../assets/image/clock.png') center/cover no-repeat;
     }
   }
-  .line_chart {
-    border: 3px solid $sub3;
-    width: 1000px;
-    height: 300px;
-    margin-left: -20px;
-    margin-top: 20px;
-    border-radius: 20px;
-    display: flex;
-    justify-content: center;
-  }
-  .chart_box {
-    width: 1000px;
-    margin-left: -20px;
-    margin-top: 20px;
-    display: flex;
-    justify-content: center;
-    .donut_chart,
-    .bar_chart {
-      display: flex;
-      justify-content: center;
-      width: 490px;
-      border: 3px solid $sub3;
-      border-radius: 20px;
-      height: 200px;
+  .wrap {
+    flex-wrap: wrap;
+    flex-direction: column;
+    margin-top: 40px;
+    padding-left: 20px;
+    > div {
+      margin-bottom: 20px;
     }
-    .donut_chart {
-      display: flex;
-      justify-content: center;
-      margin-right: 20px;
-      display: flex;
-      padding-top: 10px;
+    > div:last-child {
+      margin-bottom: 0px;
+      height: 200px;
+      background: none;
     }
   }
 }
